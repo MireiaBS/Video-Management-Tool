17b3d29bf663e7f6c5c0a25290f2e74c3c8a059e
MireiaBS <barrufetsoler.mireia@gmail.com>
2022-05-03 13:01:41 +0200

    Ex-hasta-6

--- src/films.js
+++ src/films.js
@@ -25,20 +25,55 @@ function moviesAverageOfDirector(movies, director) {
 
 // Exercise 4:  Alphabetic order by title
 function orderAlphabetically(movies) {
-  let titles = movies.map( (movie) => movie.title);
-  let result = titles.sort();
-  console.log('EXERCICE 4 ->', result);
-  return result;
+  let titles = movies.map((movie) => movie.title).
+  	sort()
+  	.slice(0, 20);
+  
+  console.log('EXERCICE 4 ->', titles);
+  return titles;
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {}
+function orderByYear(movies) { 
+  let newArray = [...movies];
+  let result = newArray
+    .sort(SortArray)
+    .sort((a,b) => a.year - b.year);
+  
+  function SortArray(a, b){
+    if (a.title < b.title) {return -1;}
+    if (a.title > b.title) {return 1;}
+    return 0;
+ }
+
+ console.log('EXERCICE 5 ->', result);
+ return result;
+}
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {}
+function moviesAverageByCategory(movies,genre) {
+  let films = movies.filter( movie => movie.genre == genre);
+  let average = films.map( (average) => average.score);
+  let result = average.reduce( (acc, item) =>  { return acc = (acc + item ) / films.length});
+  console.log('EXERCICE 6 ->', result.toFixed(2));
+    
+}
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {}
+function hoursToMinutes(movies) {
+  let newArray = [...movies];
+  let result = newArray.forEach(myFunction);
+  
+  function myFunction(item) {
+    let parts = item.duration;
+    let replaced = parts.replace(/h/,'').replace(/min/,' ');    
+    let hourToMinut = replaced[0] * 60
+    + Number( (replaced[2] + replaced[3])
+    );    
+    return (hourToMinut);
+  }; 
+	console.log('EXERCICE 6 ->', result)
+}
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear() {}
